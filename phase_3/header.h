4c3203b53da1dd022c0e76cd5a1b165dfa47a747
ucrDavid <dsilv022@ucr.edu>
2018-11-11 09:29:48 -0800

    added phase_3 dir

---
+++ phase_3/header.h
@@ -0,0 +1,23 @@
+// -*- c++ -*- 
+// heading.H
+
+using namespace std;
+
+// The following line suppresses an unused-function warning.
+#define YY_NO_UNPUT
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+#include <sstream>
+#include <cassert>
+
+// define each macro
+
+inline string itoa( int x ) {  // converts an integer to a string.
+  ostringstream s;
+  s << x;
+  return s.str();
+}
